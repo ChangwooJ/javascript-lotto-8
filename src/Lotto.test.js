25e304e613217cc860af13826214a11ad1927485
changwooj <wjdckddn99@naver.com>
2025-11-03 20:23:15 +0900

    feat(test): Lotto 클래스 테스트 코드 추가

--- src/Lotto.test.js
+++ src/Lotto.test.js
@@ -1,18 +1,34 @@
-import Lotto from "../src/Lotto";
+import Lotto from "./Lotto";
 
 describe("로또 클래스 테스트", () => {
+  // 로또 예외 테스트
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
     }).toThrow("[ERROR]");
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호에 숫자가 아닌 문자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, "a"]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 1부터 45 사이의 숫자가 아닌 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+
+  // 로또 정상 테스트
+  test('정상적인 로또 번호가 입력되면 로또 객체가 생성된다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  })
 });
